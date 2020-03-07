f13d513848309a52b5792a863cdeea65d95e03de
kuikui22 <assoonas.mm@gmail.com>
2020-03-07 17:48:29 +0800

    修改 增加循環的數字

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -10,6 +10,7 @@ export default class Helloworld extends cc.Component {
     actBtn:cc.Button = null;
 
     _repeatTimes = 4;
+    _runNum = 4;
 
     onLoad() {
         this.addEvent();
@@ -24,19 +25,21 @@ export default class Helloworld extends cc.Component {
     }
 
     private clickActBtn():void {
+        cc.log(44444);
         this._repeatTimes = 4;
         this.rollAct(4);
     }
 
 
     private rollAct(repeatTimes):void {
-
         for(let i = 0; i < this.scrollNode.length; i++) {
             this.scrollNode[i].runAction(cc.sequence(
-                cc.delayTime(i*0.5),
-                cc.moveTo(0.5, cc.v2(this.scrollNode[i].x, -100)),
+                cc.delayTime(i*0.4),
+                cc.moveTo(0.4, cc.v2(this.scrollNode[i].x, -100)),
                 cc.callFunc(function() {
                     this.scrollNode[i].y = 100;
+                    this.scrollNode[i].getComponent(cc.Label).string = this._runNum.toString();
+                    this._runNum = (this._runNum + 1 > 9) ? 0 : this._runNum + 1;
 
                     if(i === this.scrollNode.length -1 ) {
                         if(repeatTimes > 1) {
